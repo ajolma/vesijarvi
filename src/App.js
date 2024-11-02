a5a7159e507bec9febb5f8b9ed378f873d40cd53
Ari Jolma <ari.jolma@afry.com>
2024-11-02 10:09:38 +0200

    Tell if this is development environment.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import LeftPanel from './components/LeftPanel.js';
 import MyMap from './components/MyMap.js';
 
 const dev = process.env.NODE_ENV === 'development';
+if (dev) {
+    console.log("This is development environment.");
+}
 const host = 'biwatech.com';
 const path = 'vj3';
 export const server = dev ? `http://localhost:5000/${path}` : `https://${host}/${path}`;
